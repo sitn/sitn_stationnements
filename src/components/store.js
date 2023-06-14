1e54c1df7f73badfdbc8604ef9601f24a26b3d0f
Matthew Parkan <matthew.parkan@gmail.com>
2023-06-14 17:30:29 +0200

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -13,4 +13,9 @@ class Mob20 {
   }
 
 let locations = []
-locations.push()
+locations.push(new Mob20('I', 1000*Math.random()))
+locations.push(new Mob20('II', 1000*Math.random()))
+locations.push(new Mob20('III', 1000*Math.random()))
+locations.push(new Mob20('IV', 1000*Math.random()))
+locations.push(new Mob20('V', 1000*Math.random()))
+locations.push(new Mob20('VI', 1000*Math.random()))
