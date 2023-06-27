50004717b0959645d7c4506ba96426b614f23767
Matthew Parkan <matthew.parkan@gmail.com>
2023-06-27 17:07:48 +0200

    Update FormE.vue

--- src/components/FormE.vue
+++ src/components/FormE.vue
@@ -4,38 +4,28 @@
         <div class="text-h5">Étape 5: Résumé</div>
 
         <div class="row">
-            <div class="q-pa-md q-ma-none col-xs-12 col-sm-6 col-md-4"
-                v-for="(item, key) in this.project.affectations.filter(e => e.active)">
+            <div class="q-pa-md q-ma-none col-xs-12 col-sm-6 col-md-4">
                 <div class="bg-grey-2 q-pa-md q-my-sm rounded-borders">
 
                     <table>
                         <tr>
-                            <th>{{ item.name }}</th>
+                            <th>Affectation</th>
                             <th>Min.</th>
                             <th>Max.</th>
                         </tr>
 
-                        <tr>
-                            <td> {{ item.type == "Logement" ? "Besoin net réduit habitant" : "Besoin net réduit employé" }}
-                            </td>
-                            <td class="bg-light-blue-1">{{ item.reducedNetResidentNeed.min.toFixed(2) }}</td>
-                            <td class="bg-light-blue-1">{{ item.reducedNetResidentNeed.max.toFixed(2) }}</td>
+                        <tr v-for="(affectation, index) in this.project.affectations.filter(e => e.active)" :key="index">
+                            <td>{{ affectation.name }}</td>
+                            <td class="bg-light-blue-1">{{ affectation.totalNeed.min.toFixed(2) }}</td>
+                            <td class="bg-light-blue-1">{{ affectation.totalNeed.max.toFixed(2) }}</td>
                         </tr>
+
                         <tr>
-                            <td>{{ item.type == "Logement" ? "Besoin net réduit visiteur" : "Besoin net réduit client" }}
-                            </td>
-                            <td class="bg-light-blue-1">{{ item.reducedNetVisitorNeed.min.toFixed(2) }}</td>
-                            <td class="bg-light-blue-1">{{ item.reducedNetVisitorNeed.max.toFixed(2) }}</td>
+                            <td class="text-weight-bold">Total</td>
+                            <td class="bg-light-blue-1"></td>
+                            <td class="bg-light-blue-1"></td>
                         </tr>
-                        <tr>
-                            <td class="text-weight-bold">Besoin net réduit total</td>
-                            <td class="bg-light-blue-1 text-weight-bold">{{ (item.reducedNetResidentNeed.min +
-                                item.reducedNetVisitorNeed.min).toFixed(2)
-                            }}</td>
-                            <td class="bg-light-blue-1 text-weight-bold">{{ (item.reducedNetResidentNeed.max +
-                                item.reducedNetVisitorNeed.max).toFixed(2)
-                            }}</td>
-                        </tr>
+
                     </table>
 
                 </div>
@@ -74,4 +64,29 @@ export default {
 <style>
 @import "https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons";
 @import '../assets/quasar.prod.css';
+
+table {
+    border-collapse: collapse;
+    border: none;
+}
+
+th {
+    border: none;
+    background-color: #455a64;
+    color: white
+}
+
+tr {
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
+}
+
+tr:last-child {
+    border-top: 2px solid black;
+    border-bottom: 2px solid black;
+}
+
+td {
+    border: none !important;
+}
 </style>
