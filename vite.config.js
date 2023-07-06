384e2cdcba7b0e6d4fbc351bbddfa338063f2bfe
Matthew Parkan <matthew.parkan@gmail.com>
2023-07-06 11:58:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/web/stationnement/'
+  base: '/web/stationnement/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  /*
+  rollup: {
+    external: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts'],
+  },
+  */
+  /*
+  optimizeDeps: {
+    include: ['pdfmake/build/vfs_fonts'],
+  },
+  */
+  // optimizeDeps: { include: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts'] },
+  // commonjsOptions: { exclude: ['pdfmake/build/pdfmake', 'pdfmake/build/vfs_fonts'] },
+  // commonjsOptions: { }, // Edit: 
+
 })
