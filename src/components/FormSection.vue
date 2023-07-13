efa5922489f09627bb212ac5641c3707e5960cdb
Matthew Parkan <matthew.parkan@gmail.com>
2023-07-13 12:12:09 +0200

    Create FormSection.vue

---
+++ src/components/FormSection.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="row justify-center no-print q-my-xl">
+        <div class="col-xs-12 col-sm-12 col-md-9">
+
+            <div class="bg-blue-grey-7 text-white text-h5 q-pa-md q-mx-md">{{ title }}</div>
+            <div class="bg-white q-pa-md q-mx-md">
+
+                <slot name="content"></slot>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+// import { ref } from 'vue'
+
+export default {
+    name: 'FormSection',
+    components: {},
+    props: { 'title': String },
+    emits: [],
+    setup() {
+        return {
+            // model: ref(null),
+        }
+    },
+    data() {
+        return {
+            model: null,
+        }
+    },
+    computed: {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style scoped></style>
