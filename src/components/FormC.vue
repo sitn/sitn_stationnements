58bc90af07f49556ac992299872ded21025cd2bc
Matthew Parkan <matthew.parkan@gmail.com>
2023-07-13 12:23:40 +0200

    Update FormC.vue

--- src/components/FormC.vue
+++ src/components/FormC.vue
@@ -3,7 +3,7 @@
     <div class="q-pa-md">
         <!-- <div class="text-h5">3: Calcul du besoin net (article 28 RELConstr.)</div> -->
         <div class="row">
-            <div class="q-pa-md q-ma-none col-xs-12 col-sm-6 col-md-4"
+            <div class="q-pa-md q-ma-none col-xs-12 col-sm-6 col-md-6"
                 v-for="(item, key) in this.project.affectations.filter(e => e.active)">
                 <div class="bg-grey-2 q-pa-md q-my-sm rounded-borders">
 
