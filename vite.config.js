55947dbdf90a40ea1e047830ada4326b43034b36
Matthew Parkan <matthew.parkan@gmail.com>
2023-09-28 10:03:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/web/stationnement/', // For test use: '/web/stationnement/', for production use '/stationnement/'
+  publicDir: 'public',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets/*': fileURLToPath(new URL('./src/assets/', import.meta.url)),
+      '@public/*': fileURLToPath(new URL('./public/', import.meta.url)),
+      /*
+      '@assets/*': path.resolve(__dirname, 'src/assets/*'),
+      '@public/*': path.resolve(__dirname, 'public/*')
+      */
     }
   },
   /*
