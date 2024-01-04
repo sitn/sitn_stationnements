bed27279c2cd6513e1faa8a7761a4ccc4ea609c9
Matthew Parkan <matthew.parkan@gmail.com>
2024-01-04 18:00:17 +0100

    Added and restructured computation summary tables

--- src/views/FormE.vue
+++ src/views/FormE.vue
@@ -9,22 +9,32 @@
             <span class="text-body1">Veuillez compléter l'étape précédente</span>
         </q-banner>
 
+        <!-- 
+        <div>
+            {{ this.project.affectations[0].outputGroups }}
+        </div>
+        -->
+
         <div class="row" v-if="this.render">
+
+            <!-- CAR PARKINGS SUMMARY TABLE -->
             <div id="summary-container" class="col-xs-12 col-sm-6 col-md-6">
                 <div class="bg-white q-pa-md q-my-sm rounded-borders">
 
                     <table id="summary-table">
+                        <caption class="text-subtitle1">Stationnements voitures</caption>
                         <thead>
                             <tr>
                                 <th>Affectation</th>
                                 <th>Type de place</th>
-                                <th class="text-right"># Places</th>
+                                <!-- <th class="text-right"><q-icon name="directions_car" size="sm" /></th> -->
+                                <th class="text-right"><q-avatar rounded size="md" font-size="25px" color="blue-10" text-color="white" icon="directions_car" /></th>
                             </tr>
                         </thead>
                         <tbody>
                             <template v-for="item in this.project.affectations.filter(e => e.active)">
-                                <tr v-for="(subitem, iSub) in item.reducedOutput2">
-                                    <td v-if="iSub === 0" :rowspan="item.factors.length" class="">{{ item.name }}</td>
+                                <tr v-for="(subitem, iSub) in item.reducedOutput2.filter(e => e.group === 'car')">
+                                    <td v-if="iSub === 0" :rowspan="item.outputs.filter(e => e.group === 'car').length" class="">{{ item.name }}</td>
                                     <td>{{ subitem.name }}</td>
                                     <td class="bg-light-blue-1 text-right">{{ subitem.value.toFixed(2) }}</td>
                                     <!-- <td class="bg-light-blue-1 text-right">{{ Math.ceil(subitem.value) }}</td> -->
@@ -42,9 +52,127 @@
                     </table>
                 </div>
 
-                <q-btn id="print-btn" color="white" text-color="black" icon="print" label="Imprimer PDF" @click="print(project)" class="no-print" />
+
             </div>
 
+
+            <!-- BICYCLE PARKINGS SUMMARY TABLE -->
+            <div id="summary-container" class="col-xs-12 col-sm-6 col-md-6">
+                <div class="bg-white q-pa-md q-my-sm rounded-borders">
+
+                    <table id="summary-table">
+                        <caption class="text-subtitle1">Stationnements vélos (y.c. électriques &lt; 1kW)</caption>
+                        <thead>
+                            <tr>
+                                <th>Affectation</th>
+                                <th>Type de place</th>
+                                <!-- <th class="text-right"><q-icon name="directions_bike" size="sm" /></th> -->
+                                <th class="text-right"><q-avatar rounded size="md" font-size="25px" color="blue-10" text-color="white" icon="directions_bike" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <template v-for="item in this.project.affectations.filter(e => e.active)">
+                                <tr v-for="(subitem, iSub) in item.reducedOutput2.filter(e => e.group === 'bicycle')">
+                                    <td v-if="iSub === 0" :rowspan="item.outputs.filter(e => e.group === 'bicycle').length" class="">{{ item.name }}</td>
+                                    <td>{{ subitem.name }}</td>
+                                    <td class="bg-light-blue-1 text-right">{{ subitem.value.toFixed(2) }}</td>
+                                    <!-- <td class="bg-light-blue-1 text-right">{{ Math.ceil(subitem.value) }}</td> -->
+                                </tr>
+                            </template>
+                            <tr>
+                                <td class="text-weight-bold">Total (arrondi sup.)</td>
+                                <td class="text-weight-bold"></td>
+                                <td class="bg-light-blue-1 text-weight-bold text-right">
+                                    {{ Math.ceil(this.project.affectations.filter(e => e.active).map((x) =>
+                                        x.totalReducedOutput).reduce((acc, obj) => { return acc + obj }, 0)) }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+
+            <!-- MOTORCYCLE PARKINGS SUMMARY TABLE -->
+            <div id="summary-container" class="col-xs-12 col-sm-6 col-md-6">
+                <div class="bg-white q-pa-md q-my-sm rounded-borders">
+
+                    <table id="summary-table">
+                        <caption class="text-subtitle1">Stationnements deux-roues motorisés</caption>
+                        <thead>
+                            <tr>
+                                <th>Affectation</th>
+                                <th>Type de place</th>
+                                <!-- <th class="text-right"><q-icon name="motorcycle" size="sm" /></th>  -->
+                                <th class="text-right"><q-avatar rounded size="md" font-size="25px" color="blue-10" text-color="white" icon="motorcycle" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <template v-for="item in this.project.affectations.filter(e => e.active)">
+                                <tr v-for="(subitem, iSub) in item.reducedOutput2.filter(e => e.group === 'motorcycle')">
+                                    <td v-if="iSub === 0" :rowspan="item.outputs.filter(e => e.group === 'motorcycle').length" class="">{{ item.name }}</td>
+                                    <td>{{ subitem.name }}</td>
+                                    <td class="bg-light-blue-1 text-right">{{ subitem.value.toFixed(2) }}</td>
+                                    <!-- <td class="bg-light-blue-1 text-right">{{ Math.ceil(subitem.value) }}</td> -->
+                                </tr>
+                            </template>
+                            <tr>
+                                <td class="text-weight-bold">Total (arrondi sup.)</td>
+                                <td class="text-weight-bold"></td>
+                                <td class="bg-light-blue-1 text-weight-bold text-right">
+                                    {{ Math.ceil(this.project.affectations.filter(e => e.active).map((x) =>
+                                        x.totalReducedOutput).reduce((acc, obj) => { return acc + obj }, 0)) }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+
+            <!-- CHARGING STATIONS SUMMARY TABLE -->
+            <div id="summary-container" class="col-xs-12 col-sm-6 col-md-6">
+                <div class="bg-white q-pa-md q-my-sm rounded-borders">
+
+                    <table id="summary-table">
+                        <caption class="text-subtitle1">Equipements pour véhicules électriques</caption>
+                        <thead>
+                            <tr>
+                                <th>Affectation</th>
+                                <th>Type d'équipement</th>
+                                <!-- <th class="text-right"><q-icon name="ev_station" size="sm" /></th> -->
+                                <th class="text-right"><q-avatar rounded size="md" font-size="25px" color="blue-10" text-color="white" icon="ev_station" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <template v-for="item in this.project.affectations.filter(e => e.active)">
+                                <tr v-for="(subitem, iSub) in item.reducedOutput2.filter(e => e.group === 'station')">
+                                    <td v-if="iSub === 0" :rowspan="item.outputs.filter(e => e.group === 'station').length" class="">{{ item.name }}</td>
+                                    <td>{{ subitem.name }}</td>
+                                    <td class="bg-light-blue-1 text-right">{{ subitem.value.toFixed(2) }}</td>
+                                    <!-- <td class="bg-light-blue-1 text-right">{{ Math.ceil(subitem.value) }}</td> -->
+                                </tr>
+                            </template>
+                            <tr>
+                                <td class="text-weight-bold">Total (arrondi sup.)</td>
+                                <td class="text-weight-bold"></td>
+                                <td class="bg-light-blue-1 text-weight-bold text-right">
+                                    {{ Math.ceil(this.project.affectations.filter(e => e.active).map((x) =>
+                                        x.totalReducedOutput).reduce((acc, obj) => { return acc + obj }, 0)) }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row" v-if="this.render">
+            <q-btn id="print-btn" color="white" text-color="black" icon="print" label="Imprimer PDF" @click="print(project)" class="no-print" />
         </div>
 
     </div>
