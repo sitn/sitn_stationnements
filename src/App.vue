5251413f65fc45f6cebac46d221ffa03cd3217c5
Matthew Parkan <matthew.parkan@gmail.com>
2024-01-15 16:23:24 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,38 +21,31 @@
           </template>
         </FormSection>
 
-        <!-- 3.  PROPERTIES -->
-        <FormSection title="2. Spécificités du projet">
+        <!-- 2. RAW PARKING NEEDS -->
+        <FormSection title="2. Calcul du besoin brut (article 27 RELConstr.)">
           <template v-slot:content>
             <FormB :project="project" @updateProject=""></FormB>
-          </template>
-        </FormSection>
-
-        <!-- 3. RAW PARKING NEEDS -->
-        <FormSection title="3. Calcul du besoin brut (article 27 RELConstr.)">
-          <template v-slot:content>
-            <FormC :project="project" @updateProject=""></FormC>
           </template>
         </FormSection>
 
         <!-- 4. NET PARKING NEEDS -->
         <FormSection title="4. Calcul du besoin net (article 28 RELConstr.)">
           <template v-slot:content>
-            <FormD :project="project"></FormD>
+            <FormC :project="project"></FormC>
           </template>
         </FormSection>
 
         <!-- 4. REDUCED NET PARKING NEEDS -->
         <FormSection title="5. Calcul du besoin net réduit (article 29 RELConstr.)">
           <template v-slot:content>
-            <FormE :project="project"></FormE>
+            <FormD :project="project"></FormD>
           </template>
         </FormSection>
 
         <!-- 5. SUMMARY -->
         <FormSection title="6. Résumé des stationnements et équipements à réaliser">
           <template v-slot:content>
-            <FormF :project="project"></FormF>
+            <FormE :project="project"></FormE>
           </template>
         </FormSection>
 
@@ -70,7 +63,6 @@ import FormB from "./views/FormB.vue"
 import FormC from "./views/FormC.vue"
 import FormD from "./views/FormD.vue"
 import FormE from "./views/FormE.vue"
-import FormF from "./views/FormF.vue"
 
 export default {
   name: 'App',
@@ -81,7 +73,6 @@ export default {
     FormC,
     FormD,
     FormE,
-    FormF,
   },
   setup() {
     return {
