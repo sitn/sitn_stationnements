aa8359e61213204d4dc8f42a0cad02b1c877f8b2
Matthew Parkan <matthew.parkan@gmail.com>
2024-01-22 11:12:41 +0100

    Changed results table layout

--- src/views/FormD.vue
+++ src/views/FormD.vue
@@ -105,7 +105,7 @@
             <div class="row" v-if="this.render">
 
                 <!-- CAR PARKINGS SUMMARY TABLE -->
-                <div id="summary-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                <div id="summary-container" class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
                     <div class="bg-white q-pa-md q-my-sm rounded-borders">
 
                         <table class="total-row">
