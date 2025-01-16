f3d8c41c38f42662c49d52da0161123997ba0e47
Matthew Parkan <matthew.parkan@gmail.com>
2025-01-16 15:30:01 +0100

    Prettier formatting

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stationnement/', // for local use '', for test use: '/web/test_stationnement/', for production (docker) use '/stationnement/'
-  publicDir: 'public',
+  base: "/stationnement/", // for local use '', for test use: '/web/test_stationnement/', for production (docker) use '/stationnement/'
+  publicDir: "public",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets/*': fileURLToPath(new URL('./src/assets/', import.meta.url)),
-      '@public/*': fileURLToPath(new URL('./public/', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@assets/*": fileURLToPath(new URL("./src/assets/", import.meta.url)),
+      "@public/*": fileURLToPath(new URL("./public/", import.meta.url)),
       /*
       '@assets/*': path.resolve(__dirname, 'src/assets/*'),
       '@public/*': path.resolve(__dirname, 'public/*')
       */
-    }
-  }
-})
+    },
+  },
+});
